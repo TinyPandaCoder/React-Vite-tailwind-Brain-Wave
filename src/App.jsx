25ddee6d606a7a6159a080ce52e7cd5d31bedb04
Mahmoud Alaa <asticalaa1@gmail.com>
2024-08-12 15:36:43 +0300

    moved assests to src folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello World</h1>;
-}
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <ButtonGradient />
+    </>
+  );
+};
 
 export default App;
